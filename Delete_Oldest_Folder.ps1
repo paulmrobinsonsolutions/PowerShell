0f8f2cc26d313a5db395c46a67b3e08ce5229408
Paul Robinson <paulmrobinson@gmail.com>
2026-02-01 11:37:11 -0500

    Added batching capability

--- Delete_Oldest_Folder.ps1
+++ Delete_Oldest_Folder.ps1
@@ -1,4 +1,4 @@
-﻿Clear
+﻿Clear-Host
 #*********************************************
 # Description: This script will...
 #  1. Determine the oldest folder given a root folder. Ex. C:\Temp\Some_Folder
@@ -8,82 +8,105 @@
 #     then use to gage based on the number of files in each folder.
 #*********************************************
 
+<### --- BEGIN Predefined (or Input) variables --- ###>
 # 1. Specify the root folder path to target
 $path = "C:\Temp\Some_Folder"
+
+# Set batch size to diplay progress status.
+# Ex. Every x files deleted show a status update.
+[int]$batchSizeForMsgDisplay = 50
+
+# This will trigger the message display counter
+[int]$batchSizeCounter = $batchSizeForMsgDisplay
+<### --- END Predefined (or Input) variables --- ###>
+
 Write-Host 
 Write-Host "This session's processing has officially started..."
 Write-Host 
 
-# 2. Identify the folder with the oldest date
-$oldestFolderDate = (Get-Date -Format g)
-
 # Capture this lil nugget to compare and contrast when this session finishes...
 $sessionStartTime = Get-Date
 
+# 1. Get the oldest folder's modified date/time
 Write-Host "1. Getting the date of the oldest folder..."
-$currTime = Get-Date
-Write-Host "     Started: " $currTime.ToString("hh:mm:ss tt")
+Write-Host "     Started: $((Get-Date).ToString("hh:mm:ss tt"))"
 
 # Get oldest folder's last access date/time
 $oldestFolderDate = (Get-ChildItem $path -Attributes Directory | Sort-Object -Property LastWriteTime | Select-Object -First 1).LastWriteTime
 
-$currTime = Get-Date
-Write-Host "   Completed: " $currTime.ToString("hh:mm:ss tt")
-Write-Host 
+# 2. Set the threshold date x days beyond the oldest folder t othen delete verything before
+$thresholdDate = $oldestFolderDate.Date.AddDays(1)
+
+Write-Host "   Completed: $((Get-Date).ToString("hh:mm:ss tt"))"
 Write-Host "*** Oldest Folder -> $oldestFolderDate <- ***"
-
-# 3. Set the threshold date X days beyond the oldest folder then delete everything older
-$thresholdDate = $oldestFolderDate.Date
-$thresholdDate = $thresholdDate.AddDays(1)
-
 Write-Host 
-Write-Host "2. Getting count of files in the oldest folder to purge..."
-$currTime = Get-Date
-Write-Host "     Started: " $currTime.ToString("hh:mm:ss tt")
+Write-Host "2. Getting count of oflers to purge on this date..."
+Write-Host "     Started: $((Get-Date).ToString("hh:mm:ss tt"))"
 
-# 4. Get count of folders older than the $thresholdDate that will be deleted
-$itemCount = (Get-ChildItem $path -Attributes Directory | Where-Object { $_.LastWriteTime -le $thresholdDate } | Measure-Object).Count
+# 3. Get count of folders older than the $thresholdDate that will be deleted
+[int]$itemCount = (Get-ChildItem $path -Attributes Directory | Where-Object { $_.LastWriteTime -le $thresholdDate } | Measure-Object).Count
 
-$currTime = Get-Date
-Write-Host "   Completed: " $currTime.ToString("hh:mm:ss tt")
-Write-Host 
+Write-Host "   Completed: $((Get-Date).ToString("hh:mm:ss tt"))"
 Write-Host "*** No. of files to purge -> $itemCount <- ***"
 Write-Host 
 
-Write-Host "3. The purge of the files has begun..."
-$currTime = Get-Date
-Write-Host "     Started: " $currTime.ToString("hh:mm:ss tt")
+if ($itemCount -eq 0) {
+    Write-Host "No folders found to purge. Exiting script..."
+    exit
+}
 
-# 5. Perform deletion of folders older than thresholdDate
-#Get-ChildItem $path -Attributes Directory | Where-Object { $_.LastWriteTime -lt $thresholdDate } | Remove-Item -Recurse -Force
+Write-Host "4. The purge of folders/files has begun..."
+Write-Host "     Started: $((Get-Date).ToString("hh:mm:ss tt"))"
+
+# 4. Perform deletion of folders older than thresholdDate
+# Let's ensure this is always set to run with each run of this script.
+# We wouldn't want to keep endlessly keep increment each run of this script now would we?
+[int]$idx = 0
+
+# Let's capture this start time get a sense of how long this takes.
+$sessionStartTime = Get-Date
+
 Get-ChildItem -Path $path -Recurse -Directory | Where-Object {
     # Exclude files that begin with "template" and check if the file is older than the cutoff date
     $_.LastWriteTime -le $thresholdDate #-and $_.Name -notlike "*template*"  -and $_.Name -notlike "*input*" -and $_.Name -notlike "*splittext*"
 } | ForEach-Object {
-    ## Test purposes only. Or perhaps you would like to see the current VM?
-    #Write-Host $idx":" $_.Name " - " $_.LastWriteTime
+
     # Delete the file
     Remove-Item $_.FullName -Force -Recurse
     
     $idx++
+    if ($idx -ge $batchSizeCounter) {
+        $timeSpan = New-TimeSpan -Start $sessionStartTime -End (Get-Date)
+        $timeElapsed_Minutes = "{0:D2}" -f $timeSpan.TotalMinutes
+        $timeElapsed_Seconds = "{0:D2}" -f $timeSpan.Seconds
+
+        Write-Host "     Files deleted: $($batchSizeCounter)"
+        Write-Host "     Elapsed time " $($timeElapsed_Minutes) "minutes " $timeElapsed_Seconds "seconds"
+        $batchSizeCounter += $batchSizeForMsgDisplay
+    }
+
+    if ($idx -ge $itemCount) {
+        $sessionEndTime = Get-Date
+        Write-Host "   Completed at: $($sessionEndTime.ToString("hh:mm:ss tt"))"
+        Write-Host 
+        Write-Host "Alright we're one folder cleaner and free of!!!"
+        Write-Host 
+
+        $timeSpan = New-TimeSpan -Start $sessionStartTime -End $sessionEndTime
+
+        $timeHour = "{0:D2}" -f $($timeSpan.Hours)
+        $timeMinute = "{0:D2}" -f $($timeSpan.Minutes)
+        $timeSecond = "{0:D2}" -f $($timeSpan.Seconds)
+
+        Write-Host "Date of oldest folder :" $oldestFolderDate
+        Write-Host "# files in that folder:" $itemCount
+        Write-Host "Session was started at:" $sessionStartTime
+        Write-Host "Session completed at  :" $sessionEndTime
+        Write-Host "Total processing time : $($timeHour):$($timeMinute):$($timeSecond)"
+        Write-Host 
+
+        # Break out of the ForEach-Object loop
+        Exit
+    }
 }
 
-$sessionEndTime = Get-Date
-Write-Host "   Completed: " $sessionEndTime.ToString("hh:mm:ss tt")
-Write-Host 
-Write-Host "Alright we're one folder cleaner and free of!!!"
-Write-Host 
-
-$timeSpan = $sessionEndTime - $sessionStartTime
-
-$timeHour = "{0:D2}" -f $($timeSpan.Hours)
-$timeMinute = "{0:D2}" -f $($timeSpan.Minutes)
-$timeSecond = "{0:D2}" -f $($timeSpan.Seconds)
-
-Write-Host "Date of oldest folder :" $oldestFolderDate
-Write-Host "# files in that folder:" $itemCount
-Write-Host "Session was started at:" $sessionStartTime
-Write-Host "Session completed at  :" $currTime
-#NOTE: The tick ' before : ?? Well PowerShell don't like : just so ya know!
-Write-Host "Total processing time : $timeHour`:$timeMinute`:$timeSecond"
-Write-Host 
